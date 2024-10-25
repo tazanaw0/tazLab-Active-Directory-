ac056e40acd92e6407ab7fa2ec76a3d6659ca47b
Teme Azanaw <98842729+tazanaw0@users.noreply.github.com>
2024-10-25 00:08:27 -0400

    Revised syntax - Script runs, but issue with path
    Users stored in default users container

--- ranUsers.ps1
+++ ranUsers.ps1
@@ -1,41 +1,41 @@
 ﻿Import-Module ActiveDirectory
-#Set our AD Organizational Unit (OU)
-$ou = "OU=After-School Care,DC=tazlab,DC=local"
+# Set our AD Organizational Unit (OU)
+$OU = "OU=After-School Care,DC=tazlab,DC=local"
 
-#Set a default password for all new users
+# Set a default password for all new users
 $defaultPassword = "P@wn!8l!" 
 
-#Define number of users created 
+# Define number of users to create 
 $numberOfUsers = 20
 
-#Loop to create random users, stops running when i > number of users (20)
+# Loop to create random users, stops running when i > number of users (20)
 for ($i = 1; $i -le $numberOfUsers; $i++) {
-    #Define array of first and last names 
+    # Define array of first and last names 
     $firstNames = @("Jim", "Alice", "Bob", "Robert", "Karen", "Michael", "Laura") 
     $lastNames = @("Lahey", "Jones", "Mendoza", "Williams", "Evans", "Garcia", "Solomon")
 
-    #Randomly select first and last name from defined arrays
+    # Randomly select first and last name from defined arrays
     $firstName = $firstNames | Get-Random 
     $lastName = $lastNames | Get-Random
 
-    #Define variable to grab first letter of the first name 
+    # Define variable to grab first letter of the first name 
     $firstInitial = $firstName.Substring(0,1)
 
-    #Create a username using the first andlast name
+    # Create a username using the first andlast name
     $username = "$firstInitial.$lastName"
 
-    #Combine randomly selected first and last names for full name 
+    # Combine randomly selected first and last names for full name 
     $fullName = "$firstName $lastName"
 
 
-    #Creates the user in Active Directory
-    New-AdUser '
-        -Name $fullName '
-        -SamAccountName $username '
-        -UserPrincipalName "$username@tazlab.local" '  
-        -Path $ou '
-        -AccountPassword (ConvertTo-SecureString $defaultPassword -AsPlainText -Force) ' 
-        -Enabled $true #Enables activation upon creation 
+    # Creates the user in Active Directory
+    New-ADUser `
+        -Name $fullName `
+        -SamAccountName $username `
+        -UserPrincipalName "$username@tazlab.local" `  
+        -Path $OU `
+        -AccountPassword (ConvertTo-SecureString $defaultPassword -AsPlainText -Force) `
+        -Enabled $true # Enables activation upon creation 
       
 
         Write-Host "created user: $username" #Prints string to console confirming user creation
